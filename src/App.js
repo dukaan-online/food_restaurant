fe8961b6e95e5cf50affec33b858b7ae26ad9afa
mithu <mithu42329@gmail.com>
2023-06-06 22:45:55 +0530

    adding food_restaurant endpoint as home page also for git

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ function App() {
         <CartContext.Provider value={{cart,setCart}}> 
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="food_restaurant/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
